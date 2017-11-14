b986224765cea134b838a61a14e06f3b124e9e54
Georgios Kyriakou <georgios.kyriakou@coolstuff.com>
2017-11-14 10:56:56 +0100

    script to start all docker containers

---
+++ docker/dockerStartContainers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+container_names=("container1" "container2" "container3" "container4" "container5")
+echo "Starting containers"
+for name in "${container_names[@]}"; do
+     docker start "${name}"
+    if [ "$name" = "mysql" ]; then
+        docker exec -it cs-mysql bash -c "service mysql start"
+    else
+        docker exec -it "${name}" bash -c "service apache2 start"
+    fi;
+done
+docker ps -a
