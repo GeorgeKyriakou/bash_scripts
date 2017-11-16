6714d4df375ee2ad2c0d102140c252256fd0aa95
Georgios Kyriakou <georgios.kyriakou@coolstuff.com>
2017-11-16 17:44:59 +0100

    add pipe to grep IP address of sql container

--- docker/dockerStartContainers.sh
+++ docker/dockerStartContainers.sh
@@ -4,9 +4,12 @@ echo "Starting containers"
 for name in "${container_names[@]}"; do
      docker start "${name}"
     if [ "$name" = "mysql" ]; then
+        docker exec -it cs-mysql bash -c "service mysql status"
         docker exec -it cs-mysql bash -c "service mysql start"
     else
+        docker exec -it "${name}" bash -c "service apache2 status"
         docker exec -it "${name}" bash -c "service apache2 start"
     fi;
 done
 docker ps -a
+docker inspect  cs-mysql | grep IPAddress
